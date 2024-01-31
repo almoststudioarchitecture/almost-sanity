a5adbdee9d4d6ba156bf5fa26cef6dcc57093e06
taliacotton <talia.cotton@gmail.com>
2024-01-31 13:22:17 -0500

    Update sanity.client.ts

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,14 +1,14 @@
 import { createClient, type ClientConfig } from "@sanity/client";
 
 const currentDate = new Date();
-const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`;
-// const formattedDate = new Date().toISOString().slice(0, 10);
+// const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`;
+const formattedDate = new Date().toISOString().slice(0, 10);
 
 const config: ClientConfig = {
   projectId: "oogp23sh",
   dataset: "production",
   // apiVersion: formattedDate,
-  useCdn: false,
+  // useCdn: false,
 };
 
 const client = createClient(config);
