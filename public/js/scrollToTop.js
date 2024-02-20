dbe60a9f5f136c1c7bf3afb5289a5a9dcaa53dec
taliacotton <talia.cotton@gmail.com>
2024-02-19 19:20:29 -0500

    Update scrollToTop.js

--- public/js/scrollToTop.js
+++ public/js/scrollToTop.js
@@ -1,3 +1,8 @@
 window.onload = function() {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 27);
+    console.log("scrolled");
+    let projectInnerElem = document.querySelector(".projectInner");
+    if (projectInnerElem != null){
+      projectInnerElem.scrollTo(0, 0);
+    }
   };
