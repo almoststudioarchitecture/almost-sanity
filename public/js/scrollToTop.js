919cc4c13fc979a7f69aa67ecef61e54f52b2bb3
taliacotton <talia.cotton@gmail.com>
2024-02-20 09:40:55 -0500

    Update scrollToTop.js

--- public/js/scrollToTop.js
+++ public/js/scrollToTop.js
@@ -1,27 +1,27 @@
-window.onload = function() {
-  window.scrollTo(0, 0);
-  console.log("scrolled");
-  let projectInnerElem = document.querySelector(".projectInner");
-  if (projectInnerElem != null){
-    projectInnerElem.scrollTo(0, 0);
-  }
-  };
+// window.onload = function() {
+//   window.scrollTo(0, 0);
+//   console.log("scrolled");
+//   let projectInnerElem = document.querySelector(".projectInner");
+//   if (projectInnerElem != null){
+//     projectInnerElem.scrollTo(0, 0);
+//   }
+//   };
 
-  console.log("test");
+//   console.log("test");
 
-  window.scrollTo(0, 0);
-  console.log("scrolled");
-  let projectInnerElem = document.querySelector(".projectInner");
-  if (projectInnerElem != null){
-    projectInnerElem.scrollTo(0, 0);
+//   window.scrollTo(0, 0);
+//   console.log("scrolled");
+//   let projectInnerElem = document.querySelector(".projectInner");
+//   if (projectInnerElem != null){
+//     projectInnerElem.scrollTo(0, 0);
 
-    document.addEventListener("scroll", function(){
-      // console.log("scrolling inside document")
-      // console.log(window.scrollY, projectInnerElem, projectInnerElem.scrollTop)
-    })
-    projectInnerElem.addEventListener("scroll", function(){
-      // console.log("scrolling inside project inner")
-      // console.log(projectInnerElem, projectInnerElem.scrollTop);
-    })
-  }
+//     document.addEventListener("scroll", function(){
+//       // console.log("scrolling inside document")
+//       // console.log(window.scrollY, projectInnerElem, projectInnerElem.scrollTop)
+//     })
+//     projectInnerElem.addEventListener("scroll", function(){
+//       // console.log("scrolling inside project inner")
+//       // console.log(projectInnerElem, projectInnerElem.scrollTop);
+//     })
+//   }
 
