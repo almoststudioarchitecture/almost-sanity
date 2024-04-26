928f7812793844ee2d733060e87efaf943b5d464
taliacotton <talia.cotton@gmail.com>
2024-04-26 15:04:56 -0400

    usecdn false

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -1,29 +1,13 @@
 // 'use client';
 
-import Image from "next/image";
-// import Script from 'next/script';
-import { Metadata } from "next";
+
 import { getSingleProject } from "@/sanity/sanity.query";
 import type { ProjectType } from "@/types";
-import { PortableText } from "@portabletext/react";
-// import fallBackImage from "@/public/project.png";
-import AboutScript from "./ProjectScript";
 import imageUrlBuilder from '@sanity/image-url';
-import ProjectGalleryImage from "../../components/ProjectGalleryImage";
-import ProjectInfoModule from "../../components/ProjectInfoModule";
-import InteractiveLogo from "../../components/global/InteractiveLogo";
 import ProjectScrollSnap from "../../components/ProjectScrollSnap";
-import Head from 'next/head';
 import Script from 'next/script';
 
-
-
-// import { Dimensions } from 'react-native';
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode, Key } from "react";
 import './project.css'
-import { useRef, useEffect } from 'react';
-
-// const projectInnerRef = useRef(null);
 
 
 type Props = {
@@ -31,33 +15,6 @@ type Props = {
     project: string;
   };
 };
-
-// useEffect(() => {
-//   window.scrollTo(0, 0);
-// }, []);
-
-// const win = Dimensions.get('window');
-
-// Dynamic metadata for SEO
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const slug = params.project;
-//   const project: ProjectType = await getSingleProject(slug);
-
-//   console.log(project);
-  
-
-//   return {
-//     title: `Almost Studio – ${project.name}`,
-//     description: project.name,
-//     openGraph: {
-//       images:
-//         project.coverImage?.image ||
-//         "https://res.cloudinary.com/victoreke/image/upload/v1689892912/docs/project.png",
-//       title: project.name,
-//       description: project.name,
-//     },
-//   };
-// }
 
 export default async function Project({ params }: Props) {
   const slug = params.project;
@@ -78,7 +35,6 @@ export default async function Project({ params }: Props) {
     <main>
       <Script src="/js/scrollToTop.js" strategy="afterInteractive" />
       <ProjectScrollSnap project={project} />
-      {/* <InteractiveLogo /> */}
     </main>
     
     </>
--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,15 +1,13 @@
 import { createClient, type ClientConfig } from "@sanity/client";
 
 const currentDate = new Date();
-// const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`;
 const formattedDate = new Date().toISOString().slice(0, 10);
 
 const config: ClientConfig = {
   projectId: "oogp23sh",
   dataset: "production",
   apiVersion: formattedDate,
-  // apiVersion: '2024-02-19',
-  useCdn: true,
+  useCdn: false,
 };
 
 const client = createClient(config);
